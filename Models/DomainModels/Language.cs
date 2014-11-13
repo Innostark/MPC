af63887624d89152c596444dc361f17307cb184c
asad <asad@innostark.com>
2014-11-13 16:12:04 +0500

    Edmx added

---
+++ Models/DomainModels/Language.cs
@@ -0,0 +1,10 @@
+﻿namespace MPC.Models.DomainModels
+{
+    public class Language
+    {
+        public int LanguageId { get; set; }
+        public string FriendlyName { get; set; }
+        public string uiCulture { get; set; }
+        public string culture { get; set; }
+    }
+}
---
+++ Models/DomainModels/RegionalSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MPC.Models.DomainModels
+{
+    public class RegionalSettings
+    {
+        public int Laguage { get; set; }
+        public string LanguageValue { get; set; }
+        public string Color { get; set; }
+        public string ColorValue { get; set; }
+        public string Organization { get; set; }
+        public string OrganizationValue { get; set; }
+        public string State { get; set; }
+        public string StateValue { get; set; }
+        public string Centre { get; set; }
+        public string CentreValue { get; set; }
+        public string ZipCode { get; set; }
+        public string ZipCodeValue { get; set; }
+        public string Metre { get; set; }
+        public string MetreValue { get; set; }
+        public string Weight { get; set; }
+        public string WeightValue { get; set; }
+        public string PaperMeasure { get; set; }
+        public string PaperMeasureValue { get; set; }
+        public string License { get; set; }
+        public string LicenceMeasure { get; set; }
+        public short? IsSelected { get; set; }
+    }
+}
